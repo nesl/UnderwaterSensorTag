1133870ac2ed4a021aecf76d9fb9e54adab65947
eunsunlee <eunsunlee@ucla.edu>
2018-01-18 22:33:05 -0800

    Client side_valencia deployment
    Client side, Valencia deployment functional

--- main.c
+++ main.c
@@ -8,39 +8,9 @@
  $Target Device: DEVICES $
 
  ******************************************************************************
- * Copyright (C) 2016 Texas Instruments Incorporated - http://www.ti.com/
- *
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions
- *  are met:
- *
- *    Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- *    Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in the
- *    documentation and/or other materials provided with the
- *    distribution.
- *
- *    Neither the name of Texas Instruments Incorporated nor the names of
- *    its contributors may be used to endorse or promote products derived
- *    from this software without specific prior written permission.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
- *  "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
- *  LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
- *  A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
- *  OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- *  LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
- *  DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
- *  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- *  (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
- *  OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-  
+ $License: BSD3 2013 $
+ *****************************************************************************/
+
 /*******************************************************************************
  * INCLUDES
  */
@@ -54,9 +24,9 @@
 #include "icall.h"
 #include "hal_assert.h"
 #include "bcomdef.h"
-#include "peripheral.h"
-#include "spp_ble_server.h"
-#include "inc/sdi_task.h" 
+#include "central.h"
+#include "spp_ble_client.h"
+#include "inc/sdi_task.h"
 
 /* Header files required to enable instruction fetch cache */
 #include <inc/hw_memmap.h>
@@ -70,9 +40,6 @@
 bleUserCfg_t user0Cfg = BLE_USER_CFG;
 
 #endif // USE_DEFAULT_USER_CFG
-
-//#include <ti/mw/display/Display.h>
-
 
 /*******************************************************************************
  * MACROS
@@ -94,24 +61,6 @@ bleUserCfg_t user0Cfg = BLE_USER_CFG;
  * GLOBAL VARIABLES
  */
 
-#ifdef CC1350_LAUNCHXL
-#ifdef POWER_SAVING
-// Power Notify Object for wake-up callbacks
-Power_NotifyObj rFSwitchPowerNotifyObj;
-static uint8_t rFSwitchNotifyCb(uint8_t eventType, uint32_t *eventArg,
-                                uint32_t *clientArg);
-#endif //POWER_SAVING
-
-PIN_State  radCtrlState;
-PIN_Config radCtrlCfg[] = 
-{
-  Board_DIO1_RFSW   | PIN_GPIO_OUTPUT_EN | PIN_GPIO_LOW  | PIN_PUSHPULL | PIN_DRVSTR_MAX, /* RF SW Switch defaults to 2.4GHz path*/
-  Board_DIO30_SWPWR | PIN_GPIO_OUTPUT_EN | PIN_GPIO_HIGH | PIN_PUSHPULL | PIN_DRVSTR_MAX, /* Power to the RF Switch */
-  PIN_TERMINATE
-};
-PIN_Handle radCtrlHandle;
-#endif //CC1350_LAUNCHXL
-
 /*******************************************************************************
  * EXTERNS
  */
@@ -119,6 +68,7 @@ PIN_Handle radCtrlHandle;
 extern void AssertHandler(uint8 assertCause, uint8 assertSubcause);
 
 extern uint16 dispHandle;
+
 
 /*******************************************************************************
  * @fn          Main
@@ -133,7 +83,7 @@ extern uint16 dispHandle;
  *
  * @param       None.
  *
- * @return      None.   
+ * @return      None.
  */
 int main()
 {
@@ -141,17 +91,6 @@ int main()
   RegisterAssertCback(AssertHandler);
 
   PIN_init(BoardGpioInitTable);
-
-#ifdef CC1350_LAUNCHXL
-  // Enable 2.4GHz Radio
-  radCtrlHandle = PIN_open(&radCtrlState, radCtrlCfg);
-  
-#ifdef POWER_SAVING
-  Power_registerNotify(&rFSwitchPowerNotifyObj, 
-                       PowerCC26XX_ENTERING_STANDBY | PowerCC26XX_AWAKE_STANDBY,
-                       (Power_NotifyFxn) rFSwitchNotifyCb, NULL);
-#endif //POWER_SAVING
-#endif //CC1350_LAUNCHXL
 
 #ifndef POWER_SAVING
   /* Set constraints for Standby, powerdown and idle mode */
@@ -166,12 +105,13 @@ int main()
   ICall_createRemoteTasks();
 
   /* Kick off profile - Priority 3 */
-  GAPRole_createTask();
+  GAPCentralRole_createTask();
 
   /* SDI UART Example Task - Priority 2 */
-  SDITask_createTask();    
-    
-  SPPBLEServer_createTask();
+  SDITask_createTask();
+
+  /* Kick off application - Priority 1 */
+  SPPBLEClient_createTask();
 
   /* enable interrupts and start SYS/BIOS */
   BIOS_start();
@@ -205,39 +145,39 @@ void AssertHandler(uint8 assertCause, uint8 assertSubcause)
     //dispHandle = Display_open(Display_Type_LCD, NULL);
   }
 
-  //Display_print0(dispHandle, 0, 0, ">>>STACK ASSERT");
+  Display_print0(dispHandle, 0, 0, ">>>STACK ASSERT");
 
   // check the assert cause
   switch (assertCause)
   {
     case HAL_ASSERT_CAUSE_OUT_OF_MEMORY:
-      //Display_print0(dispHandle, 0, 0, "***ERROR***");
-      //Display_print0(dispHandle, 2, 0, ">> OUT OF MEMORY!");
+      Display_print0(dispHandle, 0, 0, "***ERROR***");
+      Display_print0(dispHandle, 2, 0, ">> OUT OF MEMORY!");
       break;
 
     case HAL_ASSERT_CAUSE_INTERNAL_ERROR:
       // check the subcause
       if (assertSubcause == HAL_ASSERT_SUBCAUSE_FW_INERNAL_ERROR)
       {
-        //Display_print0(dispHandle, 0, 0, "***ERROR***");
-        //Display_print0(dispHandle, 2, 0, ">> INTERNAL FW ERROR!");
+        Display_print0(dispHandle, 0, 0, "***ERROR***");
+        Display_print0(dispHandle, 2, 0, ">> INTERNAL FW ERROR!");
       }
       else
       {
-        //Display_print0(dispHandle, 0, 0, "***ERROR***");
-        //Display_print0(dispHandle, 2, 0, ">> INTERNAL ERROR!");
+        Display_print0(dispHandle, 0, 0, "***ERROR***");
+        Display_print0(dispHandle, 2, 0, ">> INTERNAL ERROR!");
       }
       break;
 
     case HAL_ASSERT_CAUSE_ICALL_ABORT:
-      //Display_print0(dispHandle, 0, 0, "***ERROR***");
-      //Display_print0(dispHandle, 2, 0, ">> ICALL ABORT!");
+      Display_print0(dispHandle, 0, 0, "***ERROR***");
+      Display_print0(dispHandle, 2, 0, ">> ICALL ABORT!");
       HAL_ASSERT_SPINLOCK;
       break;
 
     default:
-      //Display_print0(dispHandle, 0, 0, "***ERROR***");
-      //Display_print0(dispHandle, 2, 0, ">> DEFAULT SPINLOCK!");
+      Display_print0(dispHandle, 0, 0, "***ERROR***");
+      Display_print0(dispHandle, 2, 0, ">> DEFAULT SPINLOCK!");
       HAL_ASSERT_SPINLOCK;
   }
 
@@ -293,7 +233,7 @@ static uint8_t rFSwitchNotifyCb(uint8_t eventType, uint32_t *eventArg,
     // Power up RF Switch
     PIN_setOutputValue(radCtrlHandle, Board_DIO30_SWPWR, 1);
   }
-  
+
   // Notification handled successfully
   return Power_NOTIFYDONE;
 }
